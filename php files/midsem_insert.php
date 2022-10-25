610311bbacb84f1685dd99c4774f01c14727af44
jilla0701 <116227816+jilla0701@users.noreply.github.com>
2022-10-25 22:23:34 +0000

    Update midsem_insert.php

--- php files/midsem_insert.php
+++ php files/midsem_insert.php
@@ -1,18 +1,17 @@
 <?php
+ 	$servername= "localhost";
+    $username="root";
+    $password="";
+    $dbname="waterworks";
 
-$servername= "localhost";
-$username="root";
-$password="";
-$dbname="waterworks";
+
 $con = mysqli_connect($servername,$username,$password,$dbname);
 
-
-$owner_id= $_GET['owner_id'];
 $place= $_GET['place'];
 $distance_cm=$_GET['level'];
 
 
-$sql = "INSERT INTO water_level (owner_id, place, level) VALUES ('{$owner_id}', '{$place}', '{$distance_cm}')";
+$sql = "INSERT INTO water_level (owner_id, place, level) VALUES ('{$place}', '{$distance_cm}')";
 
 if (mysqli_query($con, $sql))
 	echo "New record created successfully";
