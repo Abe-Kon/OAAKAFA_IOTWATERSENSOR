49e86cbb75f949366d03dbcaeb27ab999ab74964
jilla0701 <116227816+jilla0701@users.noreply.github.com>
2022-10-26 20:29:18 +0000

    Update get.php

--- php files/get.php
+++ php files/get.php
@@ -20,12 +20,29 @@
     $q=mysqli_query($con,"select owner_id, place, level from water_level where owner_id={$_GET['owner_id']} order by owner_id DESC LIMIT 5");    
     
     $row=mysqli_fetch_object($q);
+    
+    echo "
+        <table>
+            <tr>
+                <td>col1</td>
+                <td>col1</td>
+                <td>col1</td>
+            </tr>
+    ";
     while ($row)
     {         
-        
-        echo " <br>{$row->owner_id} {$row->place} {$row->level}<br>";
+        echo "
+        <tr>
+                    <td>{$row->owner_id}</td>
+                    <td>{$row->place} </td>
+                    <td>{$row->level}</td>
+        </tr>
+
+        ";
+        //echo " <br/>{$row->owner_id} {$row->place} {$row->level}<br/>";
         $row=mysqli_fetch_object($q);
-    }       
+    } 
+    echo "</table>";     
     
 
 
