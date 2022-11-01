bb478c9e05d83bbbe8d0a9c9c9e0d1349b7e10c4
Abena <73675299+Abe-Kon@users.noreply.github.com>
2022-11-01 08:48:07 +0000

    final touches
    Pushing all updated files

--- Energy_Save_partA/energySaveA.ino
+++ Energy_Save_partA/energySaveA.ino
@@ -5,24 +5,16 @@
 
 #include <HTTPClient.h>
 #include <Wire.h> 
-#include <WebServer.h>
 #include <LiquidCrystal_I2C.h>
 
 #define USE_SERIAL Serial
 #define uS_TO_S_FACTOR 1000000ULL
 #define TIME_TO_SLEEP  4*60
-#define tankheight 100 //cm
-#include "html.h"
 
 
 void lcdprint(float distance__cm);
 void check_tank(float distance__cm);
 void sleep_wake();
-void handleNotFound();
-void handleRoot();
-void autoOP();
-void manStartOP();
-void manStopOP();
 // void tank_system();
 
 WiFiMulti wifiMulti;
@@ -34,21 +26,13 @@ int echo_pin   = 18;
 int led_pin = 23;
 int Relaypin= 4;
 int distance_cm;
-int State = 0;
-
-WebServer server(80);
 
 void setup() {
-  // declare pins as output
-  pinMode(trigger_pin, OUTPUT);
-  pinMode(echo_pin, INPUT);
-  pinMode(led_pin, OUTPUT);
-  pinMode(Relaypin, OUTPUT);
-  delay(1000);
+   USE_SERIAL.begin(115200);
 
-    USE_SERIAL.begin(115200);
-
-    USE_SERIAL.println("-----------");
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
 
     for(uint8_t t = 4; t > 0; t--) {
         USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
@@ -57,31 +41,15 @@ void setup() {
     }
 
   wifiMulti.addAP("Ttbi", "yaakonadu1"); 
-
-  while(WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.print("Connected to WiFi network with IP Address: ");
-    Serial.println(WiFi.localIP());
- 
-
-  server.on("/", handleRoot);
-  server.on("/Automatic", autoOP);
-  server.on("/Start", manStartOP);
-  server.on("/Stop", manStopOP);
-
-  server.onNotFound(handleNotFound);
-  //server.on("/client", RQ);
-
-  server.begin();
-  Serial.println("Http server started");
-
+ // declare pins as output
+  pinMode(trigger_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+  pinMode(led_pin, OUTPUT);
+  pinMode(Relaypin, OUTPUT);
+  delay(1000);
 
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
-  digitalWrite(Relaypin, HIGH);
+  digitalWrite(Relaypin, LOW);
   lcd.init(); // initialize LCD
   lcd.backlight();
   lcd.setCursor(0,0);//Set cursor value
@@ -89,11 +57,8 @@ void setup() {
   delay(500);
   lcd.clear();
 }
-}
 
 void loop() {
-  server.handleClient();
-  delay(2);
  // wait for WiFi connection
   if((wifiMulti.run() == WL_CONNECTED)) {
     digitalWrite(trigger_pin, LOW);
@@ -101,7 +66,7 @@ void loop() {
     digitalWrite(trigger_pin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigger_pin, LOW);
-    //digitalWrite(Relaypin, LOW);
+    digitalWrite(Relaypin, LOW);
     long duration = pulseIn(echo_pin, HIGH);
     distance_cm = (duration / 2) / 29.09;
 
@@ -160,95 +125,24 @@ void lcdprint(float distance__cm){
   lcd.setCursor(0,1); // set cursor to first column, second row
   lcd.print(String(distance__cm));
   lcd.print("cm");
-
+  delay(2000);
 }
 
 void check_tank(float distance__cm){
-
-  if(State == 0){
-    if (distance__cm<15.00) {
+  if(distance__cm<15.00) {
     digitalWrite(led_pin, HIGH);
     digitalWrite(Relaypin, LOW);
-    // delay(5000);
 
     if(distance__cm>=100.0){
     digitalWrite(led_pin, LOW);
     digitalWrite(Relaypin, HIGH);
-    // delay(5000);
-    //sleep_wake();  
+    sleep_wake();  
     }
-    }
-   }
-
-  else if (State == 1){
-    digitalWrite(led_pin,HIGH);
-    digitalWrite(Relaypin, LOW); 
+   
   }
-
-  else if (State == 2){
+  else{
     digitalWrite(led_pin,LOW);
     digitalWrite(Relaypin, HIGH);
-    //sleep_wake();  
-<<<<<<< Updated upstream
+    sleep_wake();  
   }
-
 }
-
-void handleNotFound(){
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-=======
->>>>>>> Stashed changes
-  }
-
- 
-
-  void handleRoot(){
-    
-    server.send(200, "text/html",page);
-    }
-
-  void autoOP (){
-    State = 0;
-  }
-
-  void manStartOP (){
-    State = 1;
-  }
-
-  void manStopOP(){
-    State = 2;
-  }
-
-//   void LEDRfunct()
-// { if (manualState == HIGH){
-//   LEDR= !LEDR;
-//   digitalWrite(LED_PIN, LEDR);
-//   //counter++;
-//   String str = "OFF";    //very little returned
-//   if(LEDR == HIGH ) str = "ON";
-//   server.send(200, "text/plain", str);
-//   Serial.println("red");
-// }
-// }
-
-// void manualfunct(){
-//  if(manualState == HIGH){
-// // manualState = !manualState
-//    manualState = LOW;
-// }
-//  else{
-//   manualState = HIGH;
-//  }
-//  }
